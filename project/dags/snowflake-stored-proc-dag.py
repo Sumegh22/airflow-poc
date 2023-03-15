fb48f32b963abf70e601c2f83291f38a573cfe4d
sumeght <=>
2023-03-15 17:22:36 +0530

    Updating Dag to execute procedure

--- project/dags/snowflake-stored-proc-dag.py
+++ project/dags/snowflake-stored-proc-dag.py
@@ -4,9 +4,7 @@ from datetime import datetime
 
 default_args = {
     'owner': 'airflow',
-    'start_date': datetime(2023, 3, 15),
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),
+    'start_date': datetime(2023, 3, 1),
 }
 
 with DAG('snowflake_stored_proc_dag', default_args=default_args, schedule_interval=None) as dag:
@@ -14,7 +12,7 @@ with DAG('snowflake_stored_proc_dag', default_args=default_args, schedule_interv
     run_stored_proc = SnowflakeOperator(
         task_id='run_stored_proc',
         snowflake_conn_id='snowflake_conn',
-        sql='CALL UPSERT_TO_RUNTIME_AUDIT();',
+        sql='CALL UPSERT_TO_PURGE_RUNTIME_AUDIT_STAGING();',
         autocommit=True
     )
 
